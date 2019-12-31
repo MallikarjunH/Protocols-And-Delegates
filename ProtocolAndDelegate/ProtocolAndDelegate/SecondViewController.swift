5342db611ca443958bb12df3bed8482fc6f68585
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-12-31 15:03:21 +0530

    Added VC

---
+++ ProtocolAndDelegate/ProtocolAndDelegate/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  ProtocolAndDelegate
+//
+//  Created by mallikarjun on 31/12/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
